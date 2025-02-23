6d32bcfc95d3b825b3749afeb11788f034c485ab
Nayane de Souza <159593131+nayanesenhorinha@users.noreply.github.com>
2025-02-23 07:29:05 -0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Meu App PWA',
-        short_name: 'AppPWA',
-        description: 'Um exemplo de PWA com React e Vite',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: 'favicon-96x96.png',
-            sizes: '96x96',
-            type: 'image/png',
-          },
-          {
-            src: 'favicon-96x96.png',
-            sizes: '96x96',
-            type: 'image/png',
-          },
-        ],
-      },
-    }),
-  ],
-});
+  plugins: [react()],
+})
